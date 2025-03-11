ba49fba396c4dbb5b179ba120105b9f9aa0a705a
TheSumSt_wincs <2240765672@qq.com>
2025-03-11 04:00:22 +0000

    feat: commit from code-server

---
+++ 1_code/0_2_test2/InitializationTest.java
@@ -0,0 +1,16 @@
+public class InitializationTest
+{
+    public static void main(String[] args)
+    {
+        int i ;
+        // i ++ ; 
+    }
+}
+
+/**
+ * PS E:\_ComputerLearning\3_Programing_OOP\1_code\0_2_test2> javac .\InitializationTest.java  
+ * .\InitializationTest.java:6: 错误: 可能尚未初始化变量i
+ *         i ++ ;
+ *         ^
+ * 1 个错误
+ */
