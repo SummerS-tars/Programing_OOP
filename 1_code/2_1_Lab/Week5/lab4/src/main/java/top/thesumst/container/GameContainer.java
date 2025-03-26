daf8f35cb8d92ffc5421b26b998151b14530af6f
TheSumSt <2240765672@qq.com>
2025-03-27 02:48:09 +0800

    lab4: feat add container package tor un the game

---
+++ 1_code/2_1_Lab/Week5/lab4/src/main/java/top/thesumst/container/GameContainer.java
@@ -0,0 +1,104 @@
+package top.thesumst.container;
+
+import top.thesumst.tools.*;
+import top.thesumst.mode.*;
+import top.thesumst.tools.command.*;
+
+public class GameContainer 
+{
+    private GameList gameList ;
+    private static int currentGameOrder ;
+    private boolean isRunning ;
+
+    public GameContainer()
+    {
+        gameList = new GameList() ;
+        currentGameOrder = 1 ;
+        isRunning = true ;
+    }
+
+    /**
+     * ! 游戏主循环
+     */
+    public void runGame()
+    {
+        ReceiveTools receiver = new ReceiveTools() ;
+
+        while(isRunning)
+        {
+            // 获取当前游戏
+            GameMode currentGame = gameList.getGame(currentGameOrder) ;
+
+            // 显示游戏信息
+            displayGameState(currentGame) ; // TODO 游戏信息显示模块
+
+            // 接收输入并执行命令
+            CommandResult result = receiver.receiveAndExecuteCommand(currentGame, gameList) ;
+
+            // 处理命令结果
+            handleCommandResult(result) ; // TODO 命令结果处理模块
+        }
+    }
+
+    /**
+     * 切换游戏
+     * @param order
+     */
+    public static void switchGameOrder(int order)
+    {
+        currentGameOrder = order ;
+    }
+
+    /**
+     * 显示当前游戏状态
+     */
+    private void displayGameState(GameMode game) {
+        PrintTools.clearConsole();
+        
+        // 显示游戏列表
+        System.out.println("----- 游戏列表 -----");
+        for (int i = 1; i <= GameList.getGameNumber(); i++) 
+        {
+            GameMode g = gameList.getGame(i);
+            String current = (i == currentGameOrder) ? "[当前] " : "       ";
+            System.out.printf("%s\n", current + g);
+        }
+        System.out.println("--------------------");
+        
+        // 显示当前游戏状态
+        game.printBoard();
+        game.printPlayerInfo();   
+    }
+
+    /**
+     * 处理命令执行结果
+     */
+    private void handleCommandResult(CommandResult result) 
+    {
+        // 处理失败情况
+        if (!result.isSuccess()) 
+        {
+            PauseTools.pause(result.getMessage() + " 请按回车键以重新输入");
+            return;
+        }
+        
+        // 显示成功消息
+        PauseTools.pause(result.getMessage() + " 请按回车键以继续");
+
+        // 处理退出命令
+        if (result.shouldQuit()) 
+        {
+            isRunning = false;
+            return;
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        // 创建游戏容器
+        GameContainer container = new GameContainer() ;
+
+
+        container.runGame() ;
+    }
+}
---
+++ 1_code/2_1_Lab/Week5/lab4/src/main/java/top/thesumst/container/GameList.java
@@ -0,0 +1,166 @@
+package top.thesumst.container;
+
+import top.thesumst.mode.GameMode;
+import top.thesumst.mode.PeaceMode;
+import top.thesumst.mode.ReversiMode;
+import top.thesumst.tools.PauseTools;
+import top.thesumst.tools.PrintTools;
+import top.thesumst.type.ChessColor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.InputMismatchException;
+
+public class GameList
+{
+    private static int gameNumber ;
+    private List<GameMode> games ;
+    private static String player1Name , player2Name ;
+    private static ChessColor player1Color , player2Color ;
+
+    public GameList()
+    {
+        gameNumber = 0 ;
+        games = new ArrayList<GameMode>() ;
+        setInitializeInfo() ;
+        addGame("peace") ;
+        addGame("reversi") ;
+    }    
+
+    /**
+     * * addGame方法用于添加游戏
+     * @param gameMode 游戏模式:
+     * 1. peace: 和平模式
+     * 2. reversi: 翻转棋
+     */
+    public void addGame(String gameMode)
+    {
+        gameNumber++ ;
+        int size = 8 ;
+        switch (gameMode) {
+            case "peace":
+                games.add(new PeaceMode(gameNumber, gameMode, size, player1Name, player2Name, player1Color, player2Color));
+                break;
+            case "reversi":
+                games.add(new ReversiMode(gameNumber, gameMode, size, player1Name, player2Name, player1Color, player2Color));
+                break;
+            default:
+                PauseTools.pause("游戏模式错误！");
+                break;
+        }
+    }
+
+    /**
+     * * getGame方法用于获取游戏
+     * @param gameNum 游戏编号
+     * @return GameMode 游戏
+     */
+    public GameMode getGame(int gameNum)
+    {
+        return games.get(gameNum - 1) ;
+    }
+
+    /**
+     * * getGameNumber方法用于获取游戏数量
+     * @return int 游戏数量
+     */
+    public static int getGameNumber()
+    {
+        return gameNumber ;
+    }
+
+    /**
+     * * setInitializeInfo方法用于设置游戏初始化信息
+     * * 包括玩家名称和棋子颜色
+     */
+    private void setInitializeInfo()
+    {
+        PrintTools printTools = new PrintTools();
+        PrintTools.clearConsole(); 
+        System.out.println("请输入1号玩家的名称：");
+        player1Name = printTools.sc.nextLine() ;
+        
+        boolean optionAvailable = false ;
+        while(!optionAvailable)
+        {
+            PrintTools.clearConsole();
+            System.out.println("请选择1号玩家的棋子颜色：(1.Black ○ | 2.White ● )");
+            try {
+                int op = printTools.sc.nextInt();
+                printTools.sc.nextLine(); // 读取缓冲区中多余的换行符
+                switch (op) {
+                    case 1:
+                        player1Color = ChessColor.BLACK;
+                        optionAvailable = true;
+                        break;
+                    case 2:
+                        player1Color = ChessColor.WHITE;
+                        optionAvailable = true;
+                        break;
+                    default:
+                        PauseTools.pause("输入无效！请输入数字1或2，请键入回车键开始重新选择");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                printTools.sc.nextLine(); // 清空缓冲区
+                PauseTools.pause("输入无效！请输入数字1或2，请键入回车键开始重新选择");
+            }
+        }
+
+        PrintTools.clearConsole();
+        System.out.println("请输入2号玩家的名称：");
+        player2Name = printTools.sc.nextLine() ;
+
+        player2Color = (player1Color == ChessColor.BLACK) ? ChessColor.WHITE : ChessColor.BLACK ;
+    }
+
+    public static void main(String[] args) 
+    {
+        GameList gameList = new GameList() ;
+        PrintTools.clearConsole();
+
+        // 测试初始化信息输入
+        System.out.println(player1Name + " " + player1Color);
+        System.out.println(player2Name + " " + player2Color);
+        System.out.println(GameList.getGameNumber());
+        PauseTools.pause("请按回车键以继续");
+        PrintTools.clearConsole();
+
+        // 测试游戏添加
+        GameMode game ;
+        game = gameList.getGame(1) ;
+        System.out.println(game);
+        game.printBoard();
+        game.printPlayerInfo();
+        PauseTools.pause("请按回车键以继续");
+        PrintTools.clearConsole();
+
+        game = gameList.getGame(2) ;
+        System.out.println(game);
+        game.printBoard();
+        game.printPlayerInfo();
+        PauseTools.pause("请按回车键以继续");
+        PrintTools.clearConsole();
+
+        // 测试额外添加
+        gameList.addGame("peace") ;
+        game = gameList.getGame(3) ;
+        System.out.println("After add peace, gameNumber : " + GameList.getGameNumber());
+        System.out.println(game);
+        game.printBoard();
+        game.printPlayerInfo();
+        PauseTools.pause("请按回车键以继续");
+        PrintTools.clearConsole();
+
+        gameList.addGame("reversi") ;
+        game = gameList.getGame(4) ;
+        System.out.println("After add peace, gameNumber : " + GameList.getGameNumber());
+        System.out.println(game);
+        game.printBoard();
+        game.printPlayerInfo();
+        PauseTools.pause("请按回车键以继续");
+        PrintTools.clearConsole();
+
+        PauseTools.pause("测试完毕，请按回车键以退出");
+        return ;
+    }
+}
