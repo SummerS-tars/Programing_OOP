dbcec613ab2de0175022154e4e12c04a8717bb96
TheSumSt <2240765672@qq.com>
2025-03-27 02:46:43 +0800

    lab4: feat add input tools

---
+++ 1_code/2_1_Lab/Week5/lab4/src/main/java/top/thesumst/tools/input/InputType.java
@@ -0,0 +1,14 @@
+package top.thesumst.tools.input;
+
+/**
+ * 表示输入命令的类型
+ */
+public enum InputType 
+{
+    CHESS_MOVE,    // 下棋
+    SWITCH_BOARD,  // 切换棋盘
+    NEW_GAME,      // 新建游戏
+    PASS,          // 跳过回合
+    QUIT,          // 退出游戏
+    INVALID        // 无效输入
+}
