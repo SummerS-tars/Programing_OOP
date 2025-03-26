d77ad393bb786b41247fae82c87dbfd3daa142df
TheSumSt <2240765672@qq.com>
2025-03-27 04:10:10 +0800

    lab4: feat move TerminalUI to test

---
+++ 1_code/2_1_Lab/Week5/lab4/src/main/java/top/thesumst/terminal/TerminalUITest.java
@@ -0,0 +1,136 @@
+package top.thesumst.terminal;
+
+import java.io.IOException;
+
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.Symbols;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import top.thesumst.container.GameContainer;
+import top.thesumst.container.GameList;
+import top.thesumst.mode.GameMode;
+import top.thesumst.mode.component.ChessBoard;
+import top.thesumst.mode.component.Player;
+import top.thesumst.type.ChessColor;
+
+import java.awt.Point;
+
+public class TerminalUITest {
+    private final Screen screen;
+    private final TextGraphics textGraphics;
+    
+    // 布局尺寸常量
+    private static final int 
+        BOARD_WIDTH = 40,
+        INFO_WIDTH = 20,
+        LIST_WIDTH = 20,
+        INPUT_HEIGHT = 4;
+
+    public TerminalUITest() throws IOException {
+        Terminal terminal = new DefaultTerminalFactory()
+            .setInitialTerminalSize(new TerminalSize(80, 40))
+            .createTerminal();
+        screen = new TerminalScreen(terminal);
+        textGraphics = screen.newTextGraphics();
+        screen.startScreen();
+    }
+
+    // 主渲染方法
+    public void render(GameContainer container) throws IOException {
+        TerminalSize size = screen.getTerminalSize();
+        
+        // 划分区域
+        renderChessBoard(container, new TerminalPosition(0, 0)); 
+        renderPlayerInfo(container, new TerminalPosition(BOARD_WIDTH, 0));
+        renderGameList(container, new TerminalPosition(BOARD_WIDTH+INFO_WIDTH, 0));
+        renderInputPanel(container, new TerminalPosition(0, size.getRows()-INPUT_HEIGHT));
+        
+        screen.refresh();
+    }
+
+    private void renderChessBoard(GameContainer container, TerminalPosition start) {
+        GameMode game = container.getCurrentGame();
+        ChessBoard board = game.getChessBoard();
+        
+        // 绘制棋盘边框
+        textGraphics.drawRectangle(
+            start,
+            new TerminalSize(BOARD_WIDTH, size.getRows()-INPUT_HEIGHT),
+            Symbols.DOUBLE_LINE_HORIZONTAL
+        );
+
+        // 绘制棋盘内容
+        for (int row = 0; row < board.size; row++) {
+            for (int col = 0; col < board.size; col++) {
+                TerminalPosition pos = new TerminalPosition(
+                    start.getColumn() + 2 + col*2,
+                    start.getRow() + 1 + row
+                );
+                
+                ChessColor color = board.getChessColor(new Point(row, col));
+                textGraphics.setCharacter(pos, color.getSymbol());
+            }
+        }
+    }
+
+
+    private void renderPlayerInfo(GameContainer container, TerminalPosition start) {
+        GameMode game = container.getCurrentGame();
+        Player current = game.isBlackTurn() ? game.player1 : game.player2;
+        
+        // 使用表格形式显示
+        textGraphics.putString(start, "当前玩家:", SGR.BOLD);
+        textGraphics.putString(start.withRelativeRow(2), 
+            String.format("名称: %s", current.getName()));
+        textGraphics.putString(start.withRelativeRow(4),
+            String.format("颜色: %c", current.getColor().getSymbol()));
+        textGraphics.putString(start.withRelativeRow(6),
+            String.format("得分: %d", current.getChessNumber()));
+    }
+
+    private void renderGameList(GameContainer container, TerminalPosition start) {
+        textGraphics.drawLine(
+            start.withColumn(start.getColumn()-1),
+            start.withRelativeRow(size.getRows()-INPUT_HEIGHT-1),
+            Symbols.DOUBLE_LINE_VERTICAL
+        );
+
+        int y = 1;
+        for (int i = 1; i <= GameList.getGameNumber(); i++) {
+            String label = container.getGame(i).toString();
+            if (i == container.getCurrentGameOrder()) {
+                textGraphics.putString(start.withRelativeRow(y++), 
+                    "▶ " + label, SGR.BOLD, SGR.REVERSE);
+            } else {
+                textGraphics.putString(start.withRelativeRow(y++), "  " + label);
+            }
+        }
+    }
+
+    private void renderInputPanel(GameContainer container, TerminalPosition start) {
+        // 绘制分割线
+        textGraphics.drawLine(
+            start.withRelativeRow(-1),
+            start.withColumn(size.getColumns()-1).withRelativeRow(-1),
+            Symbols.DOUBLE_LINE_HORIZONTAL
+        );
+    
+        // 提示信息
+        String tips = container.getCurrentGame().getInputTips();
+        textGraphics.putString(start, "输入提示: " + tips);
+        
+        // 输入框
+        textGraphics.drawRectangle(
+            start.withRelativeRow(2),
+            new TerminalSize(40, 1),
+            Symbols.SINGLE_LINE_HORIZONTAL
+        );
+    }
+}
