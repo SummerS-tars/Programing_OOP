320eda2917afba0c02dba43d255b2900cd0ef66b
TheSumSt <2240765672@qq.com>
2025-04-02 19:48:41 +0800

    lab5: feat: add GomokuMode structure

--- 1_code/2_1_Lab/Week7/lab5/src/main/java/top/thesumst/mode/GomokuMode.java
+++ 1_code/2_1_Lab/Week7/lab5/src/main/java/top/thesumst/mode/GomokuMode.java
@@ -1,6 +1,25 @@
 package top.thesumst.mode;
 
+import top.thesumst.mode.component.*;
+import top.thesumst.type.*;
+import java.awt.Point;
+
 public class GomokuMode extends GameMode
 {
+    public GomokuMode(int order, String mode, int size, String name1, String name2, ChessColor color1, ChessColor color2)
+    {
+        super(order, mode, size, name1, name2, color1, color2);
+    }
+
+    public boolean receiveOperation(Point point) 
+    {
+
+    }
+
+    public boolean receiveOperation(String operation)
+    {
+
+    }
+
     
 }
