14d961804dc285da6874f9ae7e9985fad76de888
TheSumSt <2240765672@qq.com>
2025-05-19 21:58:39 +0800

    fix(lab6-event): modify Event to better support the game info

--- 1_code/2_1_Lab/Week12/lab6/src/main/java/top/thesumst/type/Event.java
+++ 1_code/2_1_Lab/Week12/lab6/src/main/java/top/thesumst/type/Event.java
@@ -1,23 +1,41 @@
 package top.thesumst.type;
 
+import top.thesumst.core.command.GameCommand;
+
 /**
  * Event类用于表示游戏中的事件
  * 包含事件类型、事件数据、处理成功与否和消息等信息
  */
-public class Event 
+public class Event
 {
     private EventType type;
+    private String rawCommand;
+    private GameCommand command;
     private Object data;
     private Boolean handleSuccess;
     private String message;
+    
+    public String getRawCommand() {
+        return rawCommand;
+    }
+    public void setRawCommand(String rawCommand) {
+        this.rawCommand = rawCommand;
+    }
     public EventType getType() {
         return type;
     }
     public void setType(EventType type) {
         this.type = type;
     }
-    public Object getData() {
-        return data;
+    public GameCommand getCommand() {
+        return command;
+    }
+    public void setCommand(GameCommand command) {
+        this.command = command;
+    }
+    @SuppressWarnings("unchecked")
+    public <T> T getData() {
+        return (T) data;
     }
     public void setData(Object data) {
         this.data = data;
