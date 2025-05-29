d29207e8eb59721029e868dbbfe88938b710f7cc
TheSumSt <2240765672@qq.com>
2025-05-30 03:50:52 +0800

    feat(PJ): add motd of GUI and give basic alignment of GUI view

--- 1_code/2_2_PJ/project/src/main/java/top/thesumst/Main.java
+++ 1_code/2_2_PJ/project/src/main/java/top/thesumst/Main.java
@@ -1,5 +1,12 @@
 package top.thesumst;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert; // 新增导入
+import javafx.scene.control.Alert.AlertType; // 新增导入
+import javafx.stage.Stage;
 import top.thesumst.core.container.GameContainer;
 import top.thesumst.core.container.GameList;
 import top.thesumst.core.loop.GameLoop;
@@ -11,13 +18,38 @@ import top.thesumst.tools.*;
 import top.thesumst.view.ViewFactory;
 import top.thesumst.view.console.CLIPrintTools;
 
-public class Main 
+public class Main extends Application
 {
     static GameContainer gameContainer;
     static GameList gameList;
     static GameLoop gameLoop;
     static Observer observer;
-    static BaseCommandProvider cmdProvider;
+    static BaseCommandProvider cmdProvider;    /**
+     * JavaFX Application start方法
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // 首先显示MOTD对话框
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("欢迎");
+        alert.setHeaderText(null);
+        alert.setContentText("欢迎来到各种棋类游戏！" + System.lineSeparator() + System.lineSeparator() + "将来这里会提示是否加载存在的游戏存档。"); // 修改MOTD内容并使用System.lineSeparator()
+        alert.showAndWait();
+
+        // 设置窗口标题
+        primaryStage.setTitle("Board Game");
+        
+        // 加载FXML文件
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainGameView.fxml"));
+        Parent root = loader.load();
+        
+        // 创建Scene并设置到Stage
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        
+        // 显示窗口
+        primaryStage.show();
+    }
 
     /**
      * 游戏主函数
@@ -25,21 +57,28 @@ public class Main
      */
     public static void main(String[] args) 
     {
-        gameMotd();
-        try{
-            observer = ViewFactory.getView(args[0]);
-            cmdProvider = CommandProviderFactory.getCommandProvider(args[0]);
-            gameList = new GameList();
-            gameLoop = GameLoopFactory.getGameLoop(args[0], gameList, cmdProvider, observer);
-            gameContainer = new GameContainer(gameList, gameLoop, cmdProvider, observer);
-            gameContainer.runGame();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("启动模式参数错误: " + e.getMessage());
-            System.out.println("请使用 'cli' 或 'gui' 启动游戏");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            System.out.println("发生了未知错误: " + e.getMessage());
+        // 检查启动参数，如果是GUI模式则启动JavaFX应用
+        if (args.length > 0 && "gui".equals(args[0])) {
+            // 启动JavaFX应用
+            launch(args);
+        } else {
+            // 传统CLI模式
+            gameMotd();
+            try{
+                observer = ViewFactory.getView(args[0]);
+                cmdProvider = CommandProviderFactory.getCommandProvider(args[0]);
+                gameList = new GameList();
+                gameLoop = GameLoopFactory.getGameLoop(args[0], gameList, cmdProvider, observer);
+                gameContainer = new GameContainer(gameList, gameLoop, cmdProvider, observer);
+                gameContainer.runGame();
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("启动模式参数错误: " + e.getMessage());
+                System.out.println("请使用 'cli' 或 'gui' 启动游戏");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("发生了未知错误: " + e.getMessage());
+            }
         }
     }
 
---
+++ 1_code/2_2_PJ/project/src/main/java/top/thesumst/view/gui/GUIController.java
@@ -0,0 +1,20 @@
+package top.thesumst.view.gui;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * GUI控制器类，负责处理MainGameView.fxml的UI控件
+ */
+public class GUIController implements Initializable {
+
+    /**
+     * 初始化方法，在FXML加载完成后自动调用
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // MOTD已通过Alert显示，此处不再需要设置Label
+    }
+}
